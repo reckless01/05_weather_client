d14079957826fdc5f81e8668323dc3eddb4dc425
lds <lds@lds.com>
2018-06-05 14:27:08 -0400

    this is my first commit

---
+++ program.py
@@ -0,0 +1,26 @@
+
+
+def main():
+    print_the_header()
+    code = input('What zip code would you like the weather forecast for?')
+    # get html from web
+    get_html_from_web(code)
+    # parse html
+    # display forecast
+    print('hello from main')
+
+
+def print_the_header():
+    print('=====================')
+    print('     Weather App     ')
+    print('=====================')
+
+
+def get_html_from_web(zipcode):
+    url = 'https://www.wunderground.com/weather/us/in/indianapolis/{}'.format(zipcode)
+    # print(url)
+    requests
+
+
+if __name__ == '__main__':
+    main()
