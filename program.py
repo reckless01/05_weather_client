03624efc5f16f423ba00583525950e55b4bafdf5
lds <lds@lds.com>
2018-06-11 16:15:29 -0400

    Added collections, used tuple for formatting print and return

--- program.py
+++ program.py
@@ -1,16 +1,22 @@
 import requests
 import bs4
+import collections
+
+WeatherReport = collections.namedtuple('WeatherReport',
+                                       'cond, temp, scale, loc')
 
 
 def main():
     print_the_header()
     code = input('What zip code would you like the weather forecast for?')
     html = get_html_from_web(code)
-
-    # parse html
-    get_weather_from_html(html)
-    # display forecast
-    print('hello from main')
+    report = get_weather_from_html(html)
+    print('The temp in {} is {} and {} {}.'.format(
+        report.loc,
+        report.temp,
+        report.scale,
+        report.cond
+    ))
 
 
 def print_the_header():
@@ -33,8 +39,28 @@ def get_weather_from_html(html):
     condition = soup.find(class_='condition-icon').get_text()
     temp = soup.find(class_='wu-unit-temperature').find(class_='wu-value').get_text()
     scale = soup.find(class_='wu-unit-temperature').find(class_='wu-label').get_text()
-    # print(loc, condition, temp, scale)
-    print('The weather in {} is {}, and {}{}'.format(loc, condition, temp, scale))
+
+    loc = cleanup_text(loc)
+    loc = find_city_and_state_from_location(loc)
+    condition = cleanup_text(condition)
+    temp = cleanup_text(temp)
+    scale = cleanup_text(scale)
+
+    report = WeatherReport(cond=condition, temp=temp, scale=scale, loc=loc)
+    return report
+
+
+def find_city_and_state_from_location(loc: str):
+    parts = loc.split('\n')
+    return parts[0].strip()
+
+
+def cleanup_text(text):
+    if not text:
+        return text
+
+    text = text.strip()
+    return text
 
 
 if __name__ == '__main__':
